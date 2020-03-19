915cd4f5f28bd8cb76a73a019cfc7702e19d74f9
Lemuel <reyeslemuelj@gmail.com>
2020-03-18 18:12:09 -0700

    add forms, implement pug, render data

--- index.js
+++ index.js
@@ -13,25 +13,42 @@ app.set('view engine', 'pug');
 app.use('/', express.static('public'));
 app.use(express.urlencoded({extended: false}))
 
-
 app.get('/', function(req, res){
-    res.render('index')
+    db.collection('Expenses').find().toArray(function(err, expenses){
+        // console.log(expenses)
+        const theExpenses = expenses
+        const iterator = theExpenses.keys()
+
+        for (const key of iterator) {
+            console.log(key);
+        }
+
+        res.render('index', {expenses: expenses})
+    })  
 })
 
 app.post('/budget', function(req, res){
-    console.log(req.body.budget)
-    res.send(`Your budget is: ${req.body.budget}`)
+    db.collection('Expenses').insertOne({budget: req.body.budget}, function(){
+        res.redirect('/')
+    })
 })
 
 app.post('/expense', function(req, res){
-    db.collection('Expenses').insertOne({expense: req.body.expenseName, amount: req.body.expenseAmount}, function(){
-        res.send(`Thanks for submitting your expense.`)
+    db.collection('Expenses').insertOne({expense: req.body.expenseName, expenseAmount: req.body.expenseAmount}, function(){
+        res.redirect('/')
     })
 })
 
 app.post('/asset', function(req, res){
-    console.log(req.body.expenseName, req.body.expenseAmount)
+    db.collection('Expenses').insertOne({asset: req.body.assetName, assetAmount: req.body.assetAmount}, function(){
     
-    res.send(`Asset: ${req.body.assetName}, Amount: ${req.body.assetAmount} `)
+    res.redirect('/')
+    })
 })
 
+
+// 1. amounts.filter()
+// get the total of the calculation of assets - expenses
+// 2. push it total variable
+// 3. if total less or equal than budget, log = within budget
+// if total more than budget, log = over budget
