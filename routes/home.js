61c7d902e14c1dd9a8bd863d7cb6809d8680c414
Lemuel <reyeslemuelj@gmail.com>
2020-03-28 02:13:54 -0700

    add historyTotal which calculate the remaining balance left in the funds. Add balance which determines whether or not you are within budget.

--- routes/home.js
+++ routes/home.js
@@ -4,18 +4,77 @@ const router = express.Router();
 const Expenses = require('../models/Expense');
 
 // MIDDLEWARE
+
 app.set('view engine', 'pug');
 
 // READ
-// Problem why my data renders three times
+
 router.get('/', async(req,res) => {
     try {
         const documents = await Expenses.find();
-
+    // budget 
         const budgets = documents.filter(expense => expense.budget);
+    // expense, expenseAmount, asset, assetAmount
         const reports = documents.filter(expense => !expense.budget);
+    
+    /////////////////////////////////
+    // access documents   ///////////
+    const expenses = documents.map(report => report);
+    console.log(expenses)
 
-        res.render('index', { expenseReport: reports, budgets})
+    //calculates budget 
+    const budget = expenses.filter(number => number.budget)        
+    const budgetTotal = Number(budget[0].budget)
+    console.log(`Your budget is: ${budgetTotal}`)
+
+    // calculates expenses
+    const expenseNumber = expenses.reduce(function(prev, current){
+        if(current.expenseAmount) {
+            return prev += Number(current.expenseAmount)
+        }
+        return prev
+    }, 0)
+
+    console.log('Expense total:', expenseNumber)
+
+    // calculates assets
+    const assetNumber = expenses.reduce(function(prev, current){
+        if(current.assetAmount) {
+            return prev += Number(current.assetAmount)
+        }
+        return prev
+    }, 0)
+
+    console.log('Asset total:', assetNumber)
+
+    // calculates total
+    let calculateTotal = function(budgetTotal, expenseNumber, assetNumber) {
+        if(assetNumber > expenseNumber) {
+            return budgetTotal + assetNumber - expenseNumber
+        } else if(expenseNumber > assetNumber) {
+            return budgetTotal + assetNumber - expenseNumber
+        }
+    }
+    
+    let historyTotal = calculateTotal(budgetTotal, expenseNumber, assetNumber)
+
+    // calculates balance
+
+    const calculateBalance = function(budgetTotal, assetNumber, expenseNumber) {
+    // budget + income - expenses = net income
+        
+        if(budgetTotal + assetNumber - expenseNumber <= 0) {
+            return `You are over budget`
+        } else if(budgetTotal + assetNumber - expenseNumber > 0){
+            return `You are within budget`
+        }
+    }
+
+    const balance = calculateBalance(budgetTotal, assetNumber, expenseNumber)
+    console.log(balance)
+  
+    // render
+    res.render('index', { expenseReport: reports, budgets, historyTotal, balance})
     } catch(err) {
         res.json({ message: err});
     }
@@ -52,5 +111,66 @@ router.get('/delete/:postId', async(req, res) => {
         res.json({ message: err })
     }
 });
+
+// router.get('/', async(req, res) => {
+//     const documents = await Expenses.find();   
+
+//     const expenses = documents.map(report => report);
+//     console.log(expenses)
+
+//     // //calculates budget 
+//     const budget = expenses.filter(number => number.budget)        
+//     const budgetTotal = Number(budget[0].budget)
+//     console.log(`Your budget is: ${budgetTotal}`)
+
+//     // calculates expenses
+//     const expenseNumber = expenses.reduce(function(prev, current){
+//         if(current.expenseAmount) {
+//             return prev += Number(current.expenseAmount)
+//         }
+//         return prev
+//     }, 0)
+
+//     console.log('Expense total:', expenseNumber)
+
+//     //calculates assets
+//     const assetNumber = expenses.reduce(function(prev, current){
+//         if(current.assetAmount) {
+//             return prev += Number(current.assetAmount)
+//         }
+//         return prev
+//     }, 0)
+
+//     console.log('Asset total:', assetNumber)
+
+//     //calculates total
+//     let calculateTotal = function(budgetTotal, expenseNumber, assetNumber) {
+//         if(assetNumber > expenseNumber) {
+//             return budgetTotal + assetNumber - expenseNumber
+//         } else if(expenseNumber > assetNumber) {
+//             return budgetTotal + assetNumber - expenseNumber
+//         }
+//     }
+    
+//     let historyTotal = calculateTotal(budgetTotal, expenseNumber, assetNumber)
+
+//     // // calculates balance
+
+//     const calculateBalance = function(budgetTotal, assetNumber, expenseNumber) {
+//        // budget + income - expenses = net income
+        
+//         if(budgetTotal + assetNumber - expenseNumber <= 0) {
+//             return `You are over budget`
+//         } else if(budgetTotal + assetNumber - expenseNumber > 0){
+//             return `You are within budget`
+//         }
+//     }
+
+//     const balance = calculateBalance(budgetTotal, assetNumber, expenseNumber)
+//     console.log(balance)
+
+//     res.render('index', historyTotal, balance)
+// });
+
 
 module.exports = router;
